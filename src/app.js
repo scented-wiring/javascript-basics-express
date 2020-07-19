a4d3826d6f02cc9db5e210f4b164638ec7a5b73a
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-07-19 23:06:11 +0100

    completed number tests

--- src/app.js
+++ src/app.js
@@ -6,8 +6,13 @@ const { firstCharacter } = require('./lib/strings');
 const { firstCharacters } = require('./lib/strings');
 const { add } = require('./lib/numbers');
 const { subtract } = require('./lib/numbers');
+const { multiply } = require('./lib/numbers');
+const { divide } = require('./lib/numbers');
+const { remainder } = require('./lib/numbers');
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/strings/hello/:string', (req, res) => {
   res.json({ result: sayHello(req.params.string) });
@@ -44,6 +49,40 @@ app.get('/numbers/subtract/:a/from/:b', (req, res) => {
   ? res.status(400).json({ error: 'Parameters must be valid numbers.' })
   :res.json({ result: subtract(b, a) });
 });
+
+app.post('/numbers/multiply', (req, res) => {
+  if (!req.body.b) {
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  } else if (Number.isNaN(parseInt(req.body.a))) {
+    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  } else {
+    res.status(200).json({ result: multiply(req.body.a, req.body.b) });
+  }
+}) 
+
+app.post('/numbers/divide', (req, res) => {
+  if (req.body.b === 0) {
+    res.status(400).json({ error: 'Unable to divide by 0.' });
+  } else if (!req.body.b){
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  } else if (Number.isNaN(parseInt(req.body.a))) {
+    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  } else {
+    res.json({ result: divide(req.body.a, req.body.b)});
+  }
+})
+
+app.post('/numbers/remainder', (req, res) => {
+  if (req.body.b === 0) {
+    res.status(400).json({ error: 'Unable to divide by 0.' });
+  } else if (!req.body.b){
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  } else if (Number.isNaN(parseInt(req.body.a))) {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  } else {
+    res.json({ result: remainder(req.body.a, req.body.b)});
+  }
+})
 
 
 
